2d23019300ee43b0a8348d9e3e0435c17e9c81e7
nadiamartel <nadiagmartel@gmail.com>
2023-11-25 19:21:37 -0300

    fix button

--- src/components/Transactions/TransactionItem.jsx
+++ src/components/Transactions/TransactionItem.jsx
@@ -7,11 +7,13 @@ export function TransactionItem({ transaction }) {
     return (
         <li className="bg-zinc-600 text-white px-3 py-1 rounded-lg mb-2 w-full flex justify-between items-center text-sm">
             <p>{transaction.description}</p>
-            <div>
-                <span>${transaction.amount}</span>
+            <div className="flex items-center">
+                <span className="mr-2">${transaction.amount}</span>
                 <button onClick={() => {
                     deleteTransaction(transaction.id);
-                }}>
+                }}
+                className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded"
+                >
                     x
                 </button>
             </div>
